2cee86e888491e04c62398e5ee138201b5ba7c58
mamad539 <cmamad511@gmail.com>
2026-06-28 20:58:54 +0330

    Update database connection settings in config.php

--- config.php
+++ config.php
@@ -1,19 +1,22 @@
 <?php
-// config.php
-$host = 'localhost';
-$db   = 'portfolio_db';
-$user = 'root';
-$pass = '';
+$host     = 'https://jdoftcwahignczzlzzri.supabase.co/rest/v1/'; 
+$port     = '6543'; 
+$db       = 'mohammadreza-portfolio'; 
+$user     = 'mohammadreza-portfolio'; 
+$password = '@Cmamad5111'; 
+
+$dsn = "pgsql:host=$host;port=$port;dbname=$db;";
+
+$options = [
+    PDO::ATTR_ERRMODE            => PDO::ERRMODE_EXCEPTION,
+    PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC,
+    PDO::ATTR_EMULATE_PREPARES   => false,
+];
 
 try {
-    $dsn = "mysql:host=$host;dbname=$db;charset=utf8mb4";
-    // تعریف دقیق متغیر pdo$
-    $pdo = new PDO($dsn, $user, $pass, [
-        PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION,
-        PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC,
-        PDO::ATTR_EMULATE_PREPARES => false,
-    ]);
-} catch (PDOException $e) {
-    die("خطا در اتصال به دیتابیس: " . $e->getMessage());
+     $pdo = new PDO($dsn, $user, $password, $options);
+    
+} catch (\PDOException $e) {
+     throw new \PDOException($e->getMessage(), (int)$e->getCode());
 }
 ?>
